5881ae5695786a8a583764378ae478a40509e870
Stef2k16 <stefanknilling@web.de>
2021-02-08 18:44:22 +0100

    Add styling and stop button to sleep timer

--- frontend/src/views/SleepTimer.jsx
+++ frontend/src/views/SleepTimer.jsx
@@ -2,26 +2,40 @@ import React, {useState} from 'react';
 import { TimePicker } from "@material-ui/pickers";
 import Button from '@material-ui/core/Button';
 import {differenceInMilliseconds, isBefore, add} from 'date-fns'
+import '../styles/sleepTimer.css';
 
 export default  function SleepTimer() {
-    const [selectedDate, handleDateChange] = useState(add(new Date(), {minutes: 30}));
+    const [selectedDate, setSelectedDate] = useState(add(new Date(), {minutes: 30}));
+    const [timerRunning, setTimerRunning] = useState(false);
+
     const startTimer = () => {
         const timeDifference = differenceInMilliseconds(selectedDate, new Date()) + 'ms';
+        setTimerRunning(true);
         window.backend.startTimer(timeDifference);
     }
 
     return (
-        <>
+        <div className="timer-wrapper">
             <TimePicker
                 clearable
                 ampm={false}
                 label="Shutdown At"
                 value={selectedDate}
-                onChange={handleDateChange}
+                onChange={setSelectedDate}
                 minutesStep={1}
+                className="time-picker"
             />
-            <Button variant="contained" color="primary" onClick={startTimer}
-                    disabled={!selectedDate || isBefore(selectedDate, new Date())}>Start Timer</Button>
-        </>
+            {timerRunning ?
+                <Button variant="contained" color="warn" disabled={!selectedDate || isBefore(selectedDate, new Date())}>
+                    Stop Timer
+                </Button>
+                :
+                <Button variant="contained" color="primary" onClick={startTimer}
+                        disabled={!selectedDate || isBefore(selectedDate, new Date())}>
+                    Start Timer
+                </Button>
+            }
+
+        </div>
     );
 }
